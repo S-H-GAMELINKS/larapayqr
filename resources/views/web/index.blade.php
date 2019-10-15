262a2e1b23207d98e61e5b6df30a028e822d644d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-16 01:22:49 +0900

    add jumbotron for web/index

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h1>Index!</h1>
-
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Larapay for QR!</h1>
+        <p class="lead">Simple QR Payment Application!</p>
+        <hr class="my-4">
+        <p>Developed by Laravel/PAY.jp!</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Add Credit Card!</a>
+    </div>
 @endsection
